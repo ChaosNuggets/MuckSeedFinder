8582e27744e4027fdf294885d251b79297589883
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-01-01 23:11:29 -0700

    changed the version to be correct

--- MuckSeedFinder/MainClass.cs
+++ MuckSeedFinder/MainClass.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace MuckSeedFinder
 {
-    [BepInPlugin("me.chaosnuggets.muckseedfinder", "MuckSeedFinder", "1.0.0.0")]
+    [BepInPlugin("me.chaosnuggets.muckseedfinder", "MuckSeedFinder", "1.1.0.0")]
     public class MainClass : BaseUnityPlugin
     {
         public static MainClass instance;
