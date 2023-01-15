e59b9b3d43037ed29c37c495a088e33fdd3f3eb0
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-01-15 02:35:34 -0500

    added 10438 difference

--- MuckSeedFinder/CreateWorld.cs
+++ MuckSeedFinder/CreateWorld.cs
@@ -14,7 +14,7 @@ namespace MuckSeedFinder
             new int[] { 19, 38, 205, 224, 243 }
         );
         public static SeedDifferenceData god = new SeedDifferenceData(
-            new int[] { 19, 38, 1204, 1223, 1242, 1261, 1280, 1299, 7745, 7764, 9025, 9044, 9063 }
+            new int[] { 19, 38, 1204, 1223, 1242, 1261, 1280, 1299, 7745, 7764, 9025, 9044, 9063, 10438 }
         ); // This is the seed data for the seeds with spear + ancient bow
 
         public static bool hasResetSeedToPreviousGood = false;
